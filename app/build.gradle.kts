6c8eb3a1a1f0db5690d54ce8971430c6645d4e1a
boris55555 <riku.posti@disroot.org>
2025-11-30 11:39:19 +0200

    Bump version to 1.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.example.sexylauncher"
         minSdk = 31
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.11"
+        versionCode = 13
+        versionName = "1.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
