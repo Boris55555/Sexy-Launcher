08512ba2e5b274482ac4c15829a322f0873fc7ce
boris55555 <riku.posti@disroot.org>
2025-12-04 18:32:51 +0200

    feat: Release v1.16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.boris55555.sexylauncher"
         minSdk = 31
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.15"
+        versionCode = 17
+        versionName = "1.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
