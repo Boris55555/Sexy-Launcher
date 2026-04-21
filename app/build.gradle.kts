a3c7e532ca9805d2ad9801c8670036247b056487
boris55555 <riku.posti@disroot.org>
2026-04-21 21:11:04 +0300

    Bump version to 2.2.6 for clean update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.boris55555.sexylauncher"
         minSdk = 31
         targetSdk = 36
-        versionCode = 34
-        versionName = "2.2.5"
+        versionCode = 35
+        versionName = "2.2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
