3960410882739f3a314784e0bcbf0bc8a68f6a55
boris55555 <riku.posti@disroot.org>
2026-04-28 19:09:27 +0300

    Prepare for F-Droid: Remove Google Fonts and update metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.gson)
-    implementation("androidx.compose.ui:ui-text-google-fonts:1.7.5")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.foundation:foundation")
     implementation("com.google.accompanist:accompanist-drawablepainter:0.37.3")
